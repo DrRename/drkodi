28126acca76a7e6b0394828adeb855f3da2996b7
Alexander Kerner <alexkerner1000@gmail.com>
2022-12-17 21:48:11 +0100

    Add subdirs check

---
+++ src/main/java/drkodi/task/SubdirsPresentTask.java
@@ -0,0 +1,21 @@
+package drkodi.task;
+
+import drkodi.KodiUtil;
+import drkodi.data.Movie;
+import javafx.concurrent.Task;
+import lombok.RequiredArgsConstructor;
+
+import java.nio.file.Path;
+
+@RequiredArgsConstructor
+public class SubdirsPresentTask extends Task<Boolean> {
+
+    private final Movie movie;
+
+    @Override
+    protected Boolean call() throws Exception {
+        Path path = movie.getRenamingPath().getOldPath();
+        return !KodiUtil.getSubdirs(path).isEmpty();
+    }
+
+}
