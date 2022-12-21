4f9b2e4e8d87568e41a53392fbf4ed4f9ace6f8c
Alexander Kerner <alexkerner1000@gmail.com>
2022-12-21 17:01:15 +0100

    Do not offer to correct folder names for now

--- src/main/java/drkodi/ProtoTypeNormalizer.java
+++ src/main/java/drkodi/ProtoTypeNormalizer.java
@@ -1,6 +1,8 @@
 package drkodi;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public abstract class ProtoTypeNormalizer {
@@ -14,17 +16,25 @@ public abstract class ProtoTypeNormalizer {
 
     protected abstract ProtoTypeNormalizerConfiguration getConfiguration();
 
+    public ProtoTypeNormalizer(){
+
+    }
+
     public String normalize(String movieTitle){
         if(movieTitle == null)
             return null;
         String result = movieTitle;
         for(String ex : getConfiguration().getReplaceWithSpace()){
-            result = result.replace(ex, " ");
+            result = Pattern.compile(ex, Pattern.LITERAL | Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE).matcher(result)
+                    .replaceAll(Matcher.quoteReplacement(" "));
         }
         for(String ex : getConfiguration().getDelete()){
-            result = result.replace(ex, "");
+            result = Pattern.compile(ex, Pattern.LITERAL | Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE).matcher(result)
+                    .replaceAll(Matcher.quoteReplacement(""));
+
         }
-        result = result.replaceAll("\\s{2,}", " ");
+        result = result.replaceAll("\\d{2,}", "");
+        result = result.replaceAll("\\s{2,}", "");
         return result;
     }
 
